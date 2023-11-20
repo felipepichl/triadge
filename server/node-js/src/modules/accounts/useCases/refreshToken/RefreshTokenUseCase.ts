56730b85b424fede0603eb4d871e27d330ac026c
Felipe Pichl <felipe.pichl@hotmail.com>
2023-11-20 16:19:02 -0300

    feat: Implement refreshToken validation

--- server/node-js/src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ server/node-js/src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -2,11 +2,16 @@ import { inject, injectable } from 'tsyringe'
 
 import { ITokenProvider } from '@modules/accounts/providers/TokenProvider/models/ITokenProvider'
 import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository'
+import { IDateProvider } from '@shared/container/providers/DateProvider/models/IDateProvider'
+
+import { UserTokens } from '@modules/accounts/domain/UserTokens'
 
 import { IUseCase } from '@shared/core/domain/IUseCase'
+import { AppError } from '@shared/error/AppError'
 
 import { authConfig } from '@config/auth'
-const { secretRefreshToken } = authConfig
+const { secretRefreshToken, expiresInRefreshToken, expiresRefreshTokenDays } =
+  authConfig
 
 interface IRequest {
   token: string
@@ -23,17 +28,52 @@ class RefreshTokenUseCase implements IUseCase<IRequest, IResponse> {
     private tokenProvider: ITokenProvider,
     @inject('UsersTokensRepository')
     private usersTokensRepository: IUsersTokensRepository,
+    @inject('DateProvider')
+    private dateProvider: IDateProvider,
   ) {}
 
   async execute({ token }: IRequest): Promise<IResponse> {
-    const { sub: userId } = this.tokenProvider.decodeToken(
+    const { sub: userId, email } = this.tokenProvider.decodeToken(
       token,
       secretRefreshToken,
     )
 
-    await this.usersTokensRepository.findByUserIdAndRefreshToken(userId, token)
+    const userToken =
+      await this.usersTokensRepository.findByUserIdAndRefreshToken(
+        userId,
+        token,
+      )
 
-    throw new Error('Method not implemented.')
+    if (!userToken) {
+      throw new AppError('Refresh Token does not exists')
+    }
+
+    await this.usersTokensRepository.deleteById(userToken.id.toString())
+
+    const refreshToken = this.tokenProvider.encodeToken(
+      {
+        sub: userId.toString(),
+        email,
+      },
+      secretRefreshToken,
+      expiresInRefreshToken,
+    )
+
+    const refreshTokenExpiresDate = this.dateProvider.addDays(
+      expiresRefreshTokenDays,
+    )
+
+    const userTokens = UserTokens.createUserTokens({
+      userId: userId.toString(),
+      expiresDate: refreshTokenExpiresDate,
+      refreshToken,
+    })
+
+    await this.usersTokensRepository.create(userTokens)
+
+    return {
+      refreshToken,
+    }
   }
 }
 
