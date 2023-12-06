2c62ae42722830aad3af31b62bd61d5a0e9111ac
Felipe Pichl <felipe.pichl@hotmail.com>
2023-12-06 16:10:29 -0300

    feat: Implement Product/Subcategory domain

---
+++ server/node-js/src/modules/products/domain/subcategory/Subcategory.ts
@@ -0,0 +1,44 @@
+import { AggregateRoot } from '@shared/core/domain/AggregateRoot'
+import { UniqueEntityID } from '@shared/core/domain/UniqueEntityID'
+
+interface ISubcategoryProps {
+  id?: string
+  name: string
+  description: string
+  categoryId: string
+}
+
+class Subcategory extends AggregateRoot<ISubcategoryProps> {
+  constructor(props: ISubcategoryProps, id?: UniqueEntityID) {
+    super(props, id)
+  }
+
+  get name(): string {
+    return this.props.name
+  }
+
+  get description(): string {
+    return this.props.description
+  }
+
+  get categoryId(): string {
+    return this.props.categoryId
+  }
+
+  public static createCategory({
+    id,
+    name,
+    description,
+    categoryId,
+  }: ISubcategoryProps): Subcategory {
+    const subcategoryProps = {
+      name,
+      description,
+      categoryId,
+    }
+
+    return AggregateRoot.create({ props: subcategoryProps, id }, Subcategory)
+  }
+}
+
+export { Subcategory }
