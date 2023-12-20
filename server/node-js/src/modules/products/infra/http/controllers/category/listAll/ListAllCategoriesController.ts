60f37f3bec9287112b75cff91dbf808bcc21afda
Felipe Pichl <felipe.pichl@hotmail.com>
2023-12-19 23:57:05 -0300

    feat: Implement ListAllCategoriesController

---
+++ server/node-js/src/modules/products/infra/http/controllers/category/listAll/ListAllCategoriesController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+import { ListAllCategoryUseCase } from '@modules/products/useCases/category/listAll/ListAllCategoryUseCase'
+
+class ListAllCategoriesController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const listAllCategoryUseCase = container.resolve(ListAllCategoryUseCase)
+
+    const result = await listAllCategoryUseCase.execute()
+
+    return response.status(200).json(result)
+  }
+}
+
+export { ListAllCategoriesController }
