e04328409a51c331118cdaf3b563d3f9ce7f2ce0
Felipe Pichl <felipe.pichl@hotmail.com>
2023-12-21 15:50:36 -0300

    feat: Implement list all categories routes

--- server/node-js/src/modules/products/infra/http/routes/category/categories.routes.ts
+++ server/node-js/src/modules/products/infra/http/routes/category/categories.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 
-import { CreateCategoryController } from '../../controllers/category/CreateCategoryController'
+import { CreateCategoryController } from '../../controllers/category/createCategory/CreateCategoryController'
+import { ListAllCategoriesController } from '../../controllers/category/listAll/ListAllCategoriesController'
 
 const categoriesRoutes = Router()
 
 const createCategoryController = new CreateCategoryController()
+const listAllCategoriesController = new ListAllCategoriesController()
 
 categoriesRoutes.post('', createCategoryController.handle)
+categoriesRoutes.get('', listAllCategoriesController.handle)
 
 export { categoriesRoutes }
