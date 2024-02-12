ac006186d509dd937dd6b201e150d37c56b3f424
Felipe Pichl <felipe.pichl@hotmail.com>
2024-02-12 20:27:58 -0300

    feat: WIP SignIn page css

--- web/vite-web/src/pages/_layouts/auth.tsx
+++ web/vite-web/src/pages/_layouts/auth.tsx
@@ -2,7 +2,8 @@ import { Outlet } from 'react-router-dom'
 
 export default function AuthLayout() {
   return (
-    <div>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground"></div>
       <h1>Autenticação</h1>
 
       <div>
