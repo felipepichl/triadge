6d382c96e967128b5e7a5c3d7e5224d1004bdf43
Felipe Pichl <felipe.pichl@hotmail.com>
2024-02-27 17:42:56 -0300

    feat: Add Finances page and route

--- web/vite-web/src/routes/index.tsx
+++ web/vite-web/src/routes/index.tsx
@@ -3,13 +3,17 @@ import { createBrowserRouter } from 'react-router-dom'
 import AppLayout from '@/pages/_layouts/app'
 import AuthLayout from '@/pages/_layouts/auth'
 import Dashboard from '@/pages/app/dashboard'
+import { Finances } from '@/pages/app/finances'
 import SignIn from '@/pages/auth/sign-in'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    children: [{ path: '/', element: <Dashboard /> }],
+    children: [
+      { path: '/', element: <Dashboard /> },
+      { path: '/finances', element: <Finances /> },
+    ],
   },
   {
     path: '/sign-in',
