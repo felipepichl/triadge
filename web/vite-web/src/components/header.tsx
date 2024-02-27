14c93fef7de30affa4adff559d07e5724e3bce71
Felipe Pichl <felipe.pichl@hotmail.com>
2024-02-27 10:22:58 -0300

    feat: Add Responsive menu

--- web/vite-web/src/components/header.tsx
+++ web/vite-web/src/components/header.tsx
@@ -1,20 +1,12 @@
-import { DraftingCompass, MenuSquare, School, Wallet } from 'lucide-react'
-import { useState } from 'react'
+import { DraftingCompass, School, Wallet } from 'lucide-react'
 
 import { ThemeToggle } from '@/components/theme/theme-toggle'
 import { Separator } from '@/components/ui/separator'
 
 import { CustomNavlink } from './menu/custom-nav-link'
 import { ResponsiveMenu } from './menu/responsive-menu'
-import { NavLink } from './nav-link'
 
 export default function Header() {
-  const [menuOpen, setMenuOpen] = useState(true)
-
-  function toggleMenu() {
-    setMenuOpen(!menuOpen)
-  }
-
   return (
     <div className="border-b">
       <div className="flex h-16 items-center gap-6 px-6">
@@ -24,7 +16,6 @@ export default function Header() {
 
         <ResponsiveMenu />
 
-        {/* Menu para telas grandes */}
         <nav className="hidden items-center space-x-4 lg:flex lg:space-x-6">
           <CustomNavlink to="/" icon={School} description="Início" />
           <CustomNavlink to="/finances" icon={Wallet} description="Finanças" />
@@ -33,17 +24,6 @@ export default function Header() {
         <div className="ml-auto flex items-center gap-2">
           <ThemeToggle />
         </div>
-
-        {/* <nav className="flex items-center space-x-4 lg:space-x-6">
-          <NavLink to="/">
-            <School className="h-4 w-4" />
-            Inicio
-          </NavLink>
-          <NavLink to="/finances">
-            <Wallet className="h-4 w-4" />
-            Finanças
-          </NavLink>
-        </nav> */}
       </div>
     </div>
   )
--- web/vite-web/src/components/menu/responsive-menu.tsx
+++ web/vite-web/src/components/menu/responsive-menu.tsx
@@ -1,8 +1,10 @@
 import { MenuSquare, School, Wallet } from 'lucide-react'
 
+import { Button } from '../ui/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuItem,
   DropdownMenuTrigger,
 } from '../ui/dropdown-menu'
 import { CustomNavlink } from './custom-nav-link'
@@ -11,13 +13,17 @@ export function ResponsiveMenu() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <button className="lg:hidden">
+        <Button className="lg:hidden" variant="outline" size="icon">
           <MenuSquare className="h-5 w-5" />
-        </button>
+        </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="start" className="w-50">
-        <CustomNavlink to="/" icon={School} description="Início" />
-        <CustomNavlink to="/finances" icon={Wallet} description="Finanças" />
+        <DropdownMenuItem>
+          <CustomNavlink to="/" icon={School} description="Início" />
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <CustomNavlink to="/finances" icon={Wallet} description="Finanças" />
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )
