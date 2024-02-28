6cf538a8b023dbcd80b9c35eb816ac72a4bcb999
Felipe Pichl <felipe.pichl@hotmail.com>
2024-02-28 14:40:16 -0300

    feat: Done summary component

--- web/vite-web/src/components/summary.tsx
+++ web/vite-web/src/components/summary.tsx
@@ -1,4 +1,4 @@
-import { ArrowDownCircle, ArrowUpCircle, DollarSign } from 'lucide-react'
+import * as React from 'react'
 
 import {
   Card,
@@ -8,42 +8,32 @@ import {
   CardTitle,
 } from './ui/card'
 
-export function Summary() {
+type SummaryProps = {
+  color: 'default' | 'green'
+  description: string
+  icon: React.ElementType
+  iconColor: string
+  value: string
+}
+
+export function Summary({
+  color,
+  description,
+  icon: Icon,
+  iconColor,
+  value,
+}: SummaryProps) {
   return (
-    <div className="max-w-1120 mx-auto grid w-full grid-cols-3 gap-8 px-4">
-      <Card className="bg-gray-700">
-        <CardHeader className="flex flex-row items-center justify-between">
-          <CardDescription className="font-bold text-slate-200">
-            Entradas
-          </CardDescription>
-          <ArrowDownCircle size={32} color="#00b37e" />
-        </CardHeader>
-        <CardContent>
-          <CardTitle className="text-white">R$ 17.000,00</CardTitle>
-        </CardContent>
-      </Card>
-      <Card className="bg-gray-700">
-        <CardHeader className="flex flex-row items-center justify-between">
-          <CardDescription className="font-bold text-slate-200">
-            Saídas
-          </CardDescription>
-          <ArrowUpCircle size={32} color="#f75a68" />
-        </CardHeader>
-        <CardContent>
-          <CardTitle className="text-white">Saídas</CardTitle>
-        </CardContent>
-      </Card>
-      <Card className="bg-lime-600">
-        <CardHeader className="flex flex-row items-center justify-between">
-          <CardDescription className="font-bold text-slate-200">
-            Total
-          </CardDescription>
-          <DollarSign size={32} color="#fff" />
-        </CardHeader>
-        <CardContent>
-          <CardTitle className="text-white">R$ 17.000,00</CardTitle>
-        </CardContent>
-      </Card>
-    </div>
+    <Card className={color === 'green' ? 'bg-green-500' : 'bg-gray-700'}>
+      <CardHeader className="flex flex-row items-center justify-between">
+        <CardDescription className="font-bold text-slate-200">
+          {description}
+        </CardDescription>
+        <Icon size={32} color={iconColor} />
+      </CardHeader>
+      <CardContent>
+        <CardTitle className="text-white">{value}</CardTitle>
+      </CardContent>
+    </Card>
   )
 }
--- web/vite-web/src/pages/app/finances.tsx
+++ web/vite-web/src/pages/app/finances.tsx
@@ -1,5 +1,31 @@
+import { ArrowDownCircle, ArrowUpCircle, DollarSign } from 'lucide-react'
+
 import { Summary } from '@/components/summary'
 
 export function Finances() {
-  return <Summary />
+  return (
+    <div className="max-w-1120 mx-auto grid w-full grid-cols-3 gap-8 px-4">
+      <Summary
+        description="Entradas"
+        color="default"
+        icon={ArrowDownCircle}
+        iconColor="#00b37e"
+        value="R$ 17.000,00"
+      />
+      <Summary
+        description="Saídas"
+        color="default"
+        icon={ArrowUpCircle}
+        iconColor="#f75a68"
+        value="R$ 1.000,00"
+      />
+      <Summary
+        description="Total"
+        color="green"
+        icon={DollarSign}
+        iconColor="#ffff"
+        value="R$ 16.000,00"
+      />
+    </div>
+  )
 }
