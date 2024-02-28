4d09092e20a2ada41a8d088ad9c3607abe60a300
Felipe Pichl <felipe.pichl@hotmail.com>
2024-02-28 16:53:04 -0300

    feat: WIP carousel finances

--- web/vite-web/src/pages/app/finances.tsx
+++ web/vite-web/src/pages/app/finances.tsx
@@ -1,31 +1,70 @@
 import { ArrowDownCircle, ArrowUpCircle, DollarSign } from 'lucide-react'
 
 import { Summary } from '@/components/summary'
+import { Card, CardContent } from '@/components/ui/card'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
 
 export function Finances() {
   return (
-    <div className="max-w-1120 mx-auto grid w-full grid-cols-3 gap-8 px-4">
-      <Summary
-        description="Entradas"
-        color="default"
-        icon={ArrowDownCircle}
-        iconColor="#00b37e"
-        value="R$ 17.000,00"
-      />
-      <Summary
-        description="Saídas"
-        color="default"
-        icon={ArrowUpCircle}
-        iconColor="#f75a68"
-        value="R$ 1.000,00"
-      />
-      <Summary
-        description="Total"
-        color="green"
-        icon={DollarSign}
-        iconColor="#ffff"
-        value="R$ 16.000,00"
-      />
-    </div>
+    <>
+      <div
+        className="
+      max-w-1120 
+      mx-auto 
+      hidden 
+      w-full
+      grid-cols-3
+      gap-8
+      px-4
+      lg:grid
+      "
+      >
+        <Summary
+          description="Entradas"
+          color="default"
+          icon={ArrowDownCircle}
+          iconColor="#00b37e"
+          value="R$ 17.000,00"
+        />
+        <Summary
+          description="Saídas"
+          color="default"
+          icon={ArrowUpCircle}
+          iconColor="#f75a68"
+          value="R$ 1.000,00"
+        />
+        <Summary
+          description="Total"
+          color="green"
+          icon={DollarSign}
+          iconColor="#ffff"
+          value="R$ 16.000,00"
+        />
+      </div>
+
+      <Carousel>
+        <CarouselContent>
+          {Array.from({ length: 3 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <Summary
+                description="Entradas"
+                color="default"
+                icon={ArrowDownCircle}
+                iconColor="#00b37e"
+                value="R$ 17.000,00"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </>
   )
 }
