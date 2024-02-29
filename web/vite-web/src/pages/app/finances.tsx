18f10dd1b11961b13c32f0dc4dd6b27dbeb4c92a
Felipe Pichl <felipe.pichl@hotmail.com>
2024-02-29 14:12:50 -0300

    feat: WIP carousel next and previus

--- web/vite-web/src/pages/app/finances.tsx
+++ web/vite-web/src/pages/app/finances.tsx
@@ -13,7 +13,7 @@ import {
 export function Finances() {
   return (
     <>
-      <div
+      {/* <div
         className="
       max-w-1120 
       mx-auto 
@@ -46,12 +46,12 @@ export function Finances() {
           iconColor="#ffff"
           value="R$ 16.000,00"
         />
-      </div>
+      </div> */}
 
-      <Carousel>
+      <Carousel className="grid grid-rows-2">
         <CarouselContent>
           {Array.from({ length: 3 }).map((_, index) => (
-            <CarouselItem key={index}>
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3" key={index}>
               <Summary
                 description="Entradas"
                 color="default"
@@ -62,8 +62,10 @@ export function Finances() {
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+        <div className="flex items-center justify-center bg-white">
+          <CarouselPrevious />
+          <CarouselNext />
+        </div>
       </Carousel>
     </>
   )
