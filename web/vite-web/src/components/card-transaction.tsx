1897069594944b0aa3c71089b446545df74d961d
Felipe Pichl <felipe.pichl@hotmail.com>
2024-03-05 10:24:53 -0300

    feat: WIP card transction css

--- web/vite-web/src/components/card-transaction.tsx
+++ web/vite-web/src/components/card-transaction.tsx
@@ -1,24 +1,40 @@
 import { Calendar, Tag } from 'lucide-react'
 
-import { Card, CardContent, CardFooter, CardHeader } from './ui/card'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card'
 
 export function CardTransction() {
   return (
-    <Card>
+    <Card className="bg-gray-300 dark:bg-gray-700">
       <CardHeader>
-        <span>Desenvolvimento de site</span>
+        <CardDescription
+          className="
+          font-semibold 
+          text-gray-500 
+          dark:text-slate-200"
+        >
+          Desenvolvimento de site
+        </CardDescription>
       </CardHeader>
       <CardContent>
-        <span>R$ 1.200,00</span>
+        <CardTitle className={'text-green-500'}>R$ 1.200,00</CardTitle>
       </CardContent>
       <CardFooter>
-        <div>
-          <Tag size={16} />
-          <span>Venda</span>
-        </div>
-        <div>
-          <Calendar size={16} />
-          <span>13/04/2024</span>
+        <div className="grid grid-cols-2">
+          <div className="flex items-center">
+            <Tag size={16} className="mr-2" />
+            <span>Venda</span>
+          </div>
+          <div className="flex items-center">
+            <Calendar size={16} className="mr-4" />
+            <span>13/04/2024</span>
+          </div>
         </div>
       </CardFooter>
     </Card>
