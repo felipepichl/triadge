0edb7e8ed1ee1030f35f315f2b35e359b06e72d7
Felipe Pichl <felipe.pichl@hotmail.com>
2024-03-06 11:16:01 -0300

    fix: Transction table

--- web/vite-web/src/components/transaction.tsx
+++ web/vite-web/src/components/transaction.tsx
@@ -1,3 +1,6 @@
+import { ArrowRightCircle } from 'lucide-react'
+import { Link } from 'react-router-dom'
+
 import { Table, TableBody, TableHead, TableRow } from './ui/table'
 
 export function Transaction() {
@@ -9,6 +12,11 @@ export function Transaction() {
           <TableHead>R$ 1.200,00</TableHead>
           <TableHead>Venda</TableHead>
           <TableHead className="text-right">13/03/2025</TableHead>
+          <TableHead className="text-center">
+            <Link to="">
+              <ArrowRightCircle />
+            </Link>
+          </TableHead>
         </TableRow>
 
         <TableRow>
@@ -16,6 +24,11 @@ export function Transaction() {
           <TableHead>R$ 120,00</TableHead>
           <TableHead>Interno</TableHead>
           <TableHead className="text-right">13/03/2025</TableHead>
+          <TableHead className="text-center">
+            <Link to="">
+              <ArrowRightCircle />
+            </Link>
+          </TableHead>
         </TableRow>
       </TableBody>
     </Table>
