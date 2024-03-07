285503bf8cd286c3a61f8d6fc1ae7fdb824f8a84
Felipe Pichl <felipe.pichl@hotmail.com>
2024-03-07 18:04:44 -0300

    fix: Transction table responsive

--- web/vite-web/src/components/transaction.tsx
+++ web/vite-web/src/components/transaction.tsx
@@ -5,15 +5,15 @@ import { Table, TableBody, TableHead, TableRow } from './ui/table'
 
 export function Transaction() {
   return (
-    <Table className="mt-11">
+    <Table className="mt-11 hidden md:table md:basis-1/2">
       <TableBody>
         <TableRow>
           <TableHead>Desenvolvimento de site</TableHead>
           <TableHead>R$ 1.200,00</TableHead>
           <TableHead>Venda</TableHead>
           <TableHead className="text-right">13/03/2025</TableHead>
-          <TableHead className="text-center">
-            <Link to="">
+          <TableHead className="flex items-center justify-center">
+            <Link to="/finance/details">
               <ArrowRightCircle />
             </Link>
           </TableHead>
@@ -24,8 +24,8 @@ export function Transaction() {
           <TableHead>R$ 120,00</TableHead>
           <TableHead>Interno</TableHead>
           <TableHead className="text-right">13/03/2025</TableHead>
-          <TableHead className="text-center">
-            <Link to="">
+          <TableHead className="flex items-center justify-center">
+            <Link to="/finance/details">
               <ArrowRightCircle />
             </Link>
           </TableHead>
--- web/vite-web/src/pages/app/finances.tsx
+++ web/vite-web/src/pages/app/finances.tsx
@@ -66,7 +66,6 @@ export function Finances() {
       </Carousel>
 
       <Transaction />
-
       <CardTransction />
     </>
   )
