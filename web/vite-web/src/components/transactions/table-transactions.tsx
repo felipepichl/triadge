0e7d640d5fc481d0903756ae0c746f64e932c3df
Felipe Pichl <felipe.pichl@hotmail.com>
2024-03-08 16:13:31 -0300

    refactor: Update table card list

--- web/vite-web/src/components/transactions/table-transactions.tsx
+++ web/vite-web/src/components/transactions/table-transactions.tsx
@@ -41,57 +41,81 @@ export function TableTransactions({ data }: TableTransactionsData) {
           ))}
         </TableBody>
       </Table>
-      <Card className="bg-gray-300 dark:bg-gray-700 md:hidden md:basis-1/2">
-        <CardHeader>
-          <CardDescription
-            className="
-          font-semibold
-          text-gray-500 
-          dark:text-slate-200 
-          "
-          >
-            {'descrition'}
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <CardTitle className="font-bold text-green-500">{'value'}</CardTitle>
-        </CardContent>
+      <ul>
+        {data.map((transaction) => (
+          <li key={transaction.descrition} className="mb-2">
+            <Card
+              className="
+              bg-gray-300 
+                dark:bg-gray-700 
+                md:hidden 
+                md:basis-1/2
+                "
+            >
+              <CardHeader>
+                <CardDescription
+                  className="
+                    font-semibold
+                  text-gray-500 
+                  dark:text-slate-200 
+                  "
+                >
+                  {transaction.descrition}
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <CardTitle className="font-bold text-green-500">
+                  {transaction.value}
+                </CardTitle>
+              </CardContent>
 
-        <CardFooter className="flex items-center justify-between px-6 pb-6">
-          <div className="flex items-center">
-            <Tag
-              size={16}
-              className="mr-2 text-gray-500 
-            dark:text-gray-400"
-            />
-            <span
-              className="
-              font-semibold
-            text-gray-500 
-            dark:text-gray-400
-          "
-            >
-              {'category'}
-            </span>
-          </div>
-          <div className="flex items-center">
-            <Calendar
-              size={16}
-              className="mr-2 text-gray-500 
-            dark:text-gray-400"
-            />
-            <span
-              className="
-                font-semibold
-              text-gray-500 
-              dark:text-gray-400
-            "
-            >
-              13/03/2025
-            </span>
-          </div>
-        </CardFooter>
-      </Card>
+              <CardFooter
+                className="
+                  flex 
+                  items-center 
+                  justify-between 
+                  px-6 
+                  pb-6
+                "
+              >
+                <div className="flex items-center">
+                  <Tag
+                    size={16}
+                    className="mr-2 text-gray-500 dark:text-gray-400"
+                  />
+                  <span
+                    className="
+                      font-semibold
+                    text-gray-500 
+                    dark:text-gray-400
+                    "
+                  >
+                    {transaction.category}
+                  </span>
+                </div>
+                <div className="flex items-center">
+                  <Calendar
+                    size={16}
+                    className="
+                      mr-2 
+                    text-gray-500 
+                    dark:text-gray-400
+                    "
+                  />
+                  <span
+                    className="
+                    font-semibold 
+                    text-gray-500 
+                    dark:text-gray-400"
+                  >
+                    13/03/2025
+                  </span>
+                </div>
+              </CardFooter>
+            </Card>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
