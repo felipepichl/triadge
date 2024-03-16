7f0b3adcb3d70f909a828cbeac78e28d2348aad3
Felipe Pichl <felipe.pichl@hotmail.com>
2024-03-16 12:12:56 -0300

    fix: Change card transaction scroll

--- web/vite-web/src/components/transactions/card-transactions.tsx
+++ web/vite-web/src/components/transactions/card-transactions.tsx
@@ -8,90 +8,85 @@ import {
   CardHeader,
   CardTitle,
 } from '../ui/card'
-import { ScrollArea } from '../ui/scroll-area'
+import { Carousel, CarouselContent, CarouselItem } from '../ui/carousel'
 import { TransactionsData } from './transactions'
 
 export function CardTransactions({ data }: TransactionsData) {
   return (
-    <ScrollArea className="h-96 pb-16">
-      {data.map((transaction) => (
-        <Card
-          key={transaction.descrition}
-          className=" mb-2 bg-gray-300 dark:bg-gray-700"
-        >
-          {/* <Card
-                key={transaction.descrition}
-                className="
-                  mb-2 
-                bg-gray-300 
-                dark:bg-gray-700 
-                  md:hidden
-                  md:basis-1/2
-                "
-              > */}
+    <Carousel
+      opts={{
+        align: 'start',
+      }}
+      orientation="vertical"
+    >
+      <CarouselContent className="-mt-1 h-[200px]">
+        {data.map((transaction) => (
+          <CarouselItem key={transaction.descrition}>
+            <Card className=" mb-2 bg-gray-300 dark:bg-gray-700">
+              <CardHeader>
+                <CardDescription
+                  className="
+                   font-semibold
+                 text-gray-500
+                 dark:text-slate-200
+                 "
+                >
+                  {transaction.descrition}
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <CardTitle className="font-bold text-green-500">
+                  {transaction.value}
+                </CardTitle>
+              </CardContent>
 
-          <CardHeader>
-            <CardDescription
-              className="
-                  font-semibold
-                text-gray-500 
-                dark:text-slate-200 
-                "
-            >
-              {transaction.descrition}
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <CardTitle className="font-bold text-green-500">
-              {transaction.value}
-            </CardTitle>
-          </CardContent>
-
-          <CardFooter
-            className="
-                flex 
-                items-center 
-                justify-between 
-                px-6 
-                pb-6
-              "
-          >
-            <div className="flex items-center">
-              <Tag
-                size={16}
-                className="mr-2 text-gray-500 dark:text-gray-400"
-              />
-              <span
+              <CardFooter
                 className="
-                    font-semibold
-                  text-gray-500 
-                  dark:text-gray-400
-                  "
+                 flex
+                 items-center
+                 justify-between
+                 px-6
+                 pb-6
+               "
               >
-                {transaction.category}
-              </span>
-            </div>
-            <div className="flex items-center">
-              <Calendar
-                size={16}
-                className="
-                    mr-2 
-                  text-gray-500 
-                  dark:text-gray-400
-                  "
-              />
-              <span
-                className="
-                    font-semibold 
-                  text-gray-500 
-                  dark:text-gray-400"
-              >
-                13/03/2025
-              </span>
-            </div>
-          </CardFooter>
-        </Card>
-      ))}
-    </ScrollArea>
+                <div className="flex items-center">
+                  <Tag
+                    size={16}
+                    className="mr-2 text-gray-500 dark:text-gray-400"
+                  />
+                  <span
+                    className="
+                     font-semibold
+                   text-gray-500
+                   dark:text-gray-400
+                   "
+                  >
+                    {transaction.category}
+                  </span>
+                </div>
+                <div className="flex items-center">
+                  <Calendar
+                    size={16}
+                    className="
+                     mr-2
+                   text-gray-500
+                   dark:text-gray-400
+                   "
+                  />
+                  <span
+                    className="
+                     font-semibold
+                   text-gray-500
+                   dark:text-gray-400"
+                  >
+                    13/03/2025
+                  </span>
+                </div>
+              </CardFooter>
+            </Card>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
   )
 }
