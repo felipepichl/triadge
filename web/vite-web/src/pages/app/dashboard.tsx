1c54dd68a83d246109a2231039aaeaaaf2e017ca
Felipe Pichl <felipe.pichl@hotmail.com>
2024-03-19 09:50:24 -0300

    feat: Add search button responsive

--- web/vite-web/src/pages/app/dashboard.tsx
+++ web/vite-web/src/pages/app/dashboard.tsx
@@ -1,16 +1,9 @@
 import { Helmet } from 'react-helmet-async'
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-
 export default function Dashboard() {
   return (
     <>
       <Helmet title="Dashboard" />
-      <div className="mb-4 flex gap-4 lg:pt-10">
-        <Input className="flex-1" />
-        <Button className="w-40 min-w-40">Pesquisar</Button>
-      </div>
     </>
   )
 }
