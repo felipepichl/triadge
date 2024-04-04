88a7b5f77993be69b5a37a4ff166bfb226aa79cf
Felipe Pichl <felipe.pichl@hotmail.com>
2024-04-03 22:10:18 -0300

    feat: Add transaction mapper

---
+++ server/node-js/src/modules/transactions/infra/prisma/mappers/TransactionMappers.ts
@@ -0,0 +1,40 @@
+import { Transaction } from '@modules/transactions/domain/Transaction'
+import { Transaction as RawTransaction } from '@prisma/client'
+
+import { IMapper } from '@shared/core/infra/Mapper'
+
+class TransactionMappers implements IMapper<Transaction, RawTransaction> {
+  toPersistence(transaction: Transaction): Transaction {
+    return transaction
+  }
+
+  toDomain({
+    description,
+    detail,
+    type,
+    value,
+    date,
+  }: RawTransaction): Transaction {
+    return Transaction.createTransaction({
+      description,
+      detail,
+      type: type as 'income' | 'outcome',
+      value: Number(value),
+      date,
+    })
+  }
+
+  toDomainArray(rawTransactions: RawTransaction[]): Transaction[] {
+    return rawTransactions.map(this.toDomain)
+  }
+
+  getMapper(): IMapper<Transaction, RawTransaction> {
+    return TransactionMappers.getMapper()
+  }
+
+  static getMapper(): TransactionMappers {
+    return new TransactionMappers()
+  }
+}
+
+export { TransactionMappers }
