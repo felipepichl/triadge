acc27eb6d5939303d124b970ade409aafa481737
Felipe Pichl <felipe.pichl@hotmail.com>
2024-04-20 11:36:59 -0300

    feat: Add create a new transaction controller

---
+++ server/node-js/src/modules/transactions/infra/http/controllers/transaction/createTransaction/CreateTransactionController.ts
@@ -0,0 +1,24 @@
+import { CreateTransactionUseCase } from '@modules/transactions/useCases/transaction/createTransactions/CreateTransactionUseCase'
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+class CreateTransactionController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { description, type, value, transactionCategoryId } = request.body
+    const { id: userId } = request.user
+
+    const createTransactionUseCase = container.resolve(CreateTransactionUseCase)
+
+    await createTransactionUseCase.execute({
+      description,
+      type,
+      value,
+      userId,
+      transactionCategoryId,
+    })
+
+    return response.status(201).send()
+  }
+}
+
+export { CreateTransactionController }
