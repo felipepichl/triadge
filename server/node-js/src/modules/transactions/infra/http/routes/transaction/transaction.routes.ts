ffa4680439c3698d2c0f81ea373be2a64bd5d86c
Felipe Pichl <felipe.pichl@hotmail.com>
2024-04-30 12:24:39 -0300

    feat: Add list all transactions route

--- server/node-js/src/modules/transactions/infra/http/routes/transaction/transaction.routes.ts
+++ server/node-js/src/modules/transactions/infra/http/routes/transaction/transaction.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 
 import { CreateTransactionController } from '../../controllers/transaction/createTransaction/CreateTransactionController'
+import { ListAllTransactionsController } from '../../controllers/transaction/listAllTransactions/ListAllTransactionsController'
 
 const transactionsRoutes = Router()
 
 const createTransactionController = new CreateTransactionController()
+const listAllTransactionsController = new ListAllTransactionsController()
 
 transactionsRoutes.post('/', createTransactionController.handle)
+transactionsRoutes.get('/', listAllTransactionsController.handle)
 
 export { transactionsRoutes }
