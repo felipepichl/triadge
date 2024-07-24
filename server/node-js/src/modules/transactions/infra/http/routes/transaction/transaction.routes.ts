c053d335f19fa8ab67dd073382dbad3765ddbca8
Felipe Pichl <felipe.pichl@hotmail.com>
2024-07-24 12:27:10 -0300

    feat: Add listByDateRange route

--- server/node-js/src/modules/transactions/infra/http/routes/transaction/transaction.routes.ts
+++ server/node-js/src/modules/transactions/infra/http/routes/transaction/transaction.routes.ts
@@ -2,13 +2,16 @@ import { Router } from 'express'
 
 import { CreateTransactionController } from '../../controllers/transaction/createTransaction/CreateTransactionController'
 import { ListAllTransactionsController } from '../../controllers/transaction/listAllTransactions/ListAllTransactionsController'
+import { ListByDateRangeController } from '../../controllers/transaction/listByDateRange/ListByDateRangeController'
 
 const transactionsRoutes = Router()
 
 const createTransactionController = new CreateTransactionController()
 const listAllTransactionsController = new ListAllTransactionsController()
+const listByDateRangeController = new ListByDateRangeController()
 
 transactionsRoutes.post('/', createTransactionController.handle)
 transactionsRoutes.get('/', listAllTransactionsController.handle)
+transactionsRoutes.get('/', listByDateRangeController.handle)
 
 export { transactionsRoutes }
