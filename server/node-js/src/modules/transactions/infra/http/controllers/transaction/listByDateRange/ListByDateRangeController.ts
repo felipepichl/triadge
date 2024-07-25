f93779d2b474d8731157b5b96951db083526c547
Felipe Pichl <felipe.pichl@hotmail.com>
2024-07-25 11:55:36 -0300

    test: Add list by date range e2e

--- server/node-js/src/modules/transactions/infra/http/controllers/transaction/listByDateRange/ListByDateRangeController.ts
+++ server/node-js/src/modules/transactions/infra/http/controllers/transaction/listByDateRange/ListByDateRangeController.ts
@@ -4,15 +4,15 @@ import { container } from 'tsyringe'
 
 class ListByDateRangeController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { startDate, endDate } = request.params
+    const { startDate, endDate } = request.query
     const { id: userId } = request.user
 
     const listByDateRangeUseCase = container.resolve(ListByDateRangeUseCase)
 
     const result = await listByDateRangeUseCase.execute({
       userId,
-      startDate: new Date(startDate),
-      endDate: new Date(endDate),
+      startDate: new Date(String(startDate)),
+      endDate: new Date(String(endDate)),
     })
 
     return response.status(200).json(result)
