1950d345b2a456e3db93b0d9642b2fd2a5bb06af
Felipe Pichl <felipe.pichl@hotmail.com>
2024-08-01 12:19:18 -0300

    feat: add scroll-area to table transactions

--- web/vite-web/src/components/transactions/table-transactions.tsx
+++ web/vite-web/src/components/transactions/table-transactions.tsx
@@ -7,41 +7,44 @@ import { Transaction } from '@/api/list-all-transaction'
 import { priceFormatter } from '@/util/formatter'
 
 import { Pagination } from '../pagination'
+import { ScrollArea } from '../ui/scroll-area'
 import { Separator } from '../ui/separator'
 import { Table, TableBody, TableHead, TableRow } from '../ui/table'
 
 export function TableTransactions({ transactions: data }: Transaction) {
   return (
     <>
-      <Table className="hidden md:table md:basis-1/2">
+      <Table className="mt-8 hidden md:table md:basis-1/2">
         <TableBody>
-          {data.map((transaction) => {
-            const rowClass =
-              transaction.type === 'outcome' ? 'text-red-500' : ''
-            const formattedDate = format(
-              new Date(transaction.date),
-              'dd/MM/yyyy',
-            )
-            return (
-              <TableRow key={transaction._id}>
-                <TableHead className={rowClass}>
-                  {transaction.description}
-                </TableHead>
-                <TableHead className={rowClass}>
-                  {priceFormatter.format(transaction.value)}
-                </TableHead>
-                <TableHead className={rowClass}>
-                  {transaction.transactionCategory.description}
-                </TableHead>
-                <TableHead className={rowClass}>{formattedDate}</TableHead>
-                <TableHead className="flex items-center justify-center">
-                  <Link to="/finance/details">
-                    <ArrowRightCircle />
-                  </Link>
-                </TableHead>
-              </TableRow>
-            )
-          })}
+          <ScrollArea className="h-72">
+            {data.map((transaction) => {
+              const rowClass =
+                transaction.type === 'outcome' ? 'text-red-500' : ''
+              const formattedDate = format(
+                new Date(transaction.date),
+                'dd/MM/yyyy',
+              )
+              return (
+                <TableRow key={transaction._id}>
+                  <TableHead className={rowClass}>
+                    {transaction.description}
+                  </TableHead>
+                  <TableHead className={rowClass}>
+                    {priceFormatter.format(transaction.value)}
+                  </TableHead>
+                  <TableHead className={rowClass}>
+                    {transaction.transactionCategory.description}
+                  </TableHead>
+                  <TableHead className={rowClass}>{formattedDate}</TableHead>
+                  <TableHead className="flex items-center justify-center">
+                    <Link to="/finance/details">
+                      <ArrowRightCircle />
+                    </Link>
+                  </TableHead>
+                </TableRow>
+              )
+            })}
+          </ScrollArea>
         </TableBody>
       </Table>
 
