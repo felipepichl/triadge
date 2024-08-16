26892bb02c103f57594b7fc9868e59ab15f83bc7
Felipe Pichl <felipe.pichl@hotmail.com>
2024-08-16 17:11:30 -0300

    feat: transactions chart with data

--- web/vite-web/src/pages/app/dashboard.tsx
+++ web/vite-web/src/pages/app/dashboard.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react'
 import { Helmet } from 'react-helmet-async'
-import { Pie, PieChart, Sector } from 'recharts'
-import { PieSectorDataItem } from 'recharts/types/polar/Pie'
+import { Pie, PieChart } from 'recharts'
 
 import {
   Card,
@@ -13,30 +12,12 @@ import {
 import {
   ChartConfig,
   ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
+  ChartLegend,
+  ChartLegendContent,
 } from '@/components/ui/chart'
 import { Separator } from '@/components/ui/separator'
 import { useAuth } from '@/hooks/use-auth'
 import { useTransaction } from '@/hooks/use-transaction'
-
-const chartConfig = {
-  transactions: {
-    label: 'Transactions',
-  },
-  income: {
-    label: 'Entrada',
-    color: '#00b37e',
-  },
-  outcome: {
-    label: 'Saída',
-    color: '#ff0000',
-  },
-  total: {
-    label: 'Total',
-    color: '#374151',
-  },
-} satisfies ChartConfig
 
 export function Dashboard() {
   const { user } = useAuth()
@@ -50,31 +31,45 @@ export function Dashboard() {
 
   const chartData = [
     {
-      type: 'income',
+      transactionType: 'income',
       value: transactionByDateRangeAndType?.balance?.income,
       fill: '#00b37e',
     },
     {
-      type: 'outcome',
+      transactionType: 'outcome',
       value: transactionByDateRangeAndType?.balance?.outcome,
       fill: '#ff0000',
     },
     {
-      type: 'total',
+      transactionType: 'total',
       value: transactionByDateRangeAndType?.balance?.total,
       fill: '#374151',
     },
   ]
 
+  const chartConfig = {
+    income: {
+      label: 'Entrada',
+      color: '#00b37e',
+    },
+    outcome: {
+      label: 'Saída',
+      color: '#ff0000',
+    },
+    total: {
+      label: 'Total',
+      color: '#374151',
+    },
+  } satisfies ChartConfig
+
   return (
     <>
       <Helmet title="Dashboard" />
-      <CardTitle className="p-4">Olá, {user?.name}</CardTitle>
+      <CardTitle className="pb-6 pt-4">Olá, {user?.name}</CardTitle>
 
       <Card className="mx-auto flex aspect-square max-h-[400px] flex-col">
-        <CardHeader className="items-center pb-0">
+        <CardHeader className="">
           <CardTitle>Transações</CardTitle>
-          <CardDescription>Agosto - Setembro 2024 </CardDescription>
         </CardHeader>
         <Separator />
         <CardContent className="flex-1 pb-0">
@@ -83,23 +78,10 @@ export function Dashboard() {
             className="mx-auto aspect-square max-h-[250px]"
           >
             <PieChart>
-              <ChartTooltip
-                cursor={false}
-                content={<ChartTooltipContent hideLabel />}
-              />
-              <Pie
-                data={chartData}
-                dataKey="value"
-                nameKey="type"
-                innerRadius={60}
-                strokeWidth={5}
-                activeIndex={0}
-                activeShape={({
-                  outerRadius = 0,
-                  ...props
-                }: PieSectorDataItem) => (
-                  <Sector {...props} outerRadius={outerRadius + 10} />
-                )}
+              <Pie data={chartData} dataKey="value" />
+              <ChartLegend
+                content={<ChartLegendContent nameKey="transactionType" />}
+                className="-translate-y-2 flex-wrap gap-2 [&>*]:basis-1/4 [&>*]:justify-center"
               />
             </PieChart>
           </ChartContainer>
