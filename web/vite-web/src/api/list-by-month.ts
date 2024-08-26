8eee54c8fc0a58a583aee112fca96be907e933b4
Felipe Pichl <felipe.pichl@hotmail.com>
2024-08-26 18:09:44 -0300

    feat: listByMonth api

---
+++ web/vite-web/src/api/list-by-month.ts
@@ -0,0 +1,55 @@
+import { api } from '@/lib/axios'
+
+export type ListByMonthBody = {
+  month: number
+}
+
+type TransactionResponse = {
+  transactions: {
+    _id: string
+    props: {
+      description: string
+      type: string
+      value: number
+      date: Date
+    }
+  }[]
+}
+
+export type Transaction = {
+  transactions: {
+    _id: string
+    description: string
+    type: string
+    value: number
+    date: Date
+  }[]
+  balance?: {
+    income: number
+    outcome: number
+  }
+}
+
+export async function apiListByType({
+  month,
+}: ListByMonthBody): Promise<Transaction> {
+  const { data } = await api.get<TransactionResponse>('/transactions/month', {
+    params: { month },
+  })
+
+  // console.log(JSON.stringify(data, null, 2))
+
+  const transactions = data.transactions.map(
+    ({ _id, props: { description, type, value, date } }) => ({
+      _id,
+      description,
+      type,
+      value,
+      date,
+    }),
+  )
+
+  return {
+    transactions,
+  }
+}
