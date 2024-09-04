e6a61d5508cb353aa9810dfbae0b08455064a073
Felipe Pichl <felipe.pichl@hotmail.com>
2024-09-04 19:13:21 -0300

    feat: repository in memory to FinancialCategory

---
+++ server/node-js/src/modules/financialCategory/repositories/in-memory/FinancialCategoryRepositoryInMemor.ts
@@ -0,0 +1,31 @@
+import { FinancialCategory } from '@modules/financialCategory/domain/FinancialCategory'
+
+import { IFinancialCategoryRepository } from '../IFinancialCategoryRepository'
+
+class FinancialCategoryRepositoryInMemor
+  implements IFinancialCategoryRepository
+{
+  private financialCategory: FinancialCategory[] = []
+
+  async create(financialCategory: FinancialCategory): Promise<void> {
+    this.financialCategory.push(financialCategory)
+  }
+
+  async listAll(): Promise<FinancialCategory[]> {
+    return this.financialCategory
+  }
+
+  async findByDescription(description: string): Promise<FinancialCategory> {
+    return this.financialCategory.find(
+      (financialCategory) => financialCategory.description === description,
+    )
+  }
+
+  async findById(id: string): Promise<FinancialCategory> {
+    return this.financialCategory.find(
+      (financialCategory) => financialCategory.id.toString() === id,
+    )
+  }
+}
+
+export { FinancialCategoryRepositoryInMemor }
