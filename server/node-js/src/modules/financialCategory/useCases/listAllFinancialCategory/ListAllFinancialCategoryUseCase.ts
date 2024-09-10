1d2250f9dac588d206cfbff0deff83d53f3ba190
Felipe Pichl <felipe.pichl@hotmail.com>
2024-09-09 22:24:18 -0300

    feat: list all to FinancialCategory

---
+++ server/node-js/src/modules/financialCategory/useCases/listAllFinancialCategory/ListAllFinancialCategoryUseCase.ts
@@ -0,0 +1,26 @@
+import { FinancialCategory } from '@modules/financialCategory/domain/FinancialCategory'
+import { IFinancialCategoryRepository } from '@modules/financialCategory/repositories/IFinancialCategoryRepository'
+import { IUseCase } from '@shared/core/domain/IUseCase'
+import { inject, injectable } from 'tsyringe'
+
+interface IResponse {
+  financialCategories: FinancialCategory[]
+}
+
+@injectable()
+class ListAllFinancialCategoryUseCase implements IUseCase<void, IResponse> {
+  constructor(
+    @inject('FinancialCategoryRepository')
+    private financialCategoryRepository: IFinancialCategoryRepository,
+  ) {}
+
+  async execute(): Promise<IResponse> {
+    const financialCategories = await this.financialCategoryRepository.listAll()
+
+    return {
+      financialCategories,
+    }
+  }
+}
+
+export { ListAllFinancialCategoryUseCase }
