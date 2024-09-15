da55b13e5bfbb995f8477c5ec474cae881f0204d
Felipe Pichl <felipe.pichl@hotmail.com>
2024-09-14 22:13:20 -0300

    feat: create controller to FinancialCategory

---
+++ server/node-js/src/modules/financialCategory/infra/http/createFinancialCategory/CreateFinancialCategoryController.ts
@@ -0,0 +1,21 @@
+import { CreateFinancialCategoryUseCase } from '@modules/financialCategory/useCases/createFinancialCategory/CreateFinancialCategoryUseCase'
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+class CreateFinancialCategoryController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { description } = request.body
+
+    const createFinancialCategoryUseCase = container.resolve(
+      CreateFinancialCategoryUseCase,
+    )
+
+    await createFinancialCategoryUseCase.execute({
+      description,
+    })
+
+    return response.status(201).send()
+  }
+}
+
+export { CreateFinancialCategoryController }
