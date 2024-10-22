9963acf9fc89b36a30bd9563710d96b01c998120
Felipe Pichl <felipe.pichl@hotmail.com>
2024-10-22 18:08:20 -0300

    test: add subcategory to transaction

--- server/node-js/src/modules/transactions/infra/http/controllers/transaction/createTransaction/CreateTransactionController.e2e-spec.ts
+++ server/node-js/src/modules/transactions/infra/http/controllers/transaction/createTransaction/CreateTransactionController.e2e-spec.ts
@@ -38,13 +38,35 @@ async function createFinancialCategory(): Promise<string> {
   return _id
 }
 
+async function createSubcategory(parentCategoryId: string): Promise<string> {
+  const token = await authenticateUser()
+
+  await request(app)
+    .post('/financial-category/subcategory')
+    .set({ Authorization: `Bearer ${token}` })
+    .send({
+      description: 'Subcategory',
+      parentCategoryId,
+    })
+
+  const response = await request(app)
+    .get(`/financial-category/subcategory/${parentCategoryId}`)
+    .set({ Authorization: `Bearer ${token}` })
+
+  const { _id } = response.body.subcategories[0]
+
+  return _id
+}
+
 describe('[E2E] - Create Transaction', () => {
   let token: string
   let financialCategoryId: string
+  let subcategoryId: string
 
   beforeEach(async () => {
     token = await authenticateUser()
     financialCategoryId = await createFinancialCategory()
+    subcategoryId = await createSubcategory(financialCategoryId)
   })
 
   it('should be to create a new transaction', async () => {
@@ -57,6 +79,21 @@ describe('[E2E] - Create Transaction', () => {
         type: 'income',
         value: 100,
         financialCategoryId,
+      })
+    expect(response.status).toBe(201)
+  })
+
+  it('should be to create a new transaction with subcategory', async () => {
+    const response = await request(app)
+      .post('/transactions')
+      .set({ Authorization: `Bearer ${token}` })
+      .send({
+        description: 'Description',
+        detail: 'Detail',
+        type: 'income',
+        value: 100,
+        financialCategoryId,
+        subcategoryId,
       })
     expect(response.status).toBe(201)
   })
