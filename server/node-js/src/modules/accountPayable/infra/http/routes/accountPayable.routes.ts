a27ab5fb9b30564b6ef9497be4c073a8186c97b5
Felipe Pichl <felipe.pichl@hotmail.com>
2024-12-20 15:20:52 -0300

    feat: accounts payable fixed routes

--- server/node-js/src/modules/accountPayable/infra/http/routes/accountPayable.routes.ts
+++ server/node-js/src/modules/accountPayable/infra/http/routes/accountPayable.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express'
 
 import { CreateAccountsPayableController } from '../controllers/createAccountsPayable/CreateAccountsPayableController'
+import { CreateFixedAccountPayableController } from '../controllers/createFixedAccountPayable/CreateFixedAccountPayableController'
 
 const accounsPayableRoutes = Router()
 
 const createAccountsPayableController = new CreateAccountsPayableController()
+const createFixedAccountsPayableController =
+  new CreateFixedAccountPayableController()
 
 accounsPayableRoutes.post('', createAccountsPayableController.handle)
+accounsPayableRoutes.post('/fixed', createFixedAccountsPayableController.handle)
 
 export { accounsPayableRoutes }
