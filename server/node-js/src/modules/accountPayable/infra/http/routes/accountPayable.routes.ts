78c3967ec404b46253f57af3faf6334eb8d3d81e
Felipe Pichl <felipe.pichl@hotmail.com>
2025-01-12 01:23:25 -0300

    feat: list all unfixed account by month route

--- server/node-js/src/modules/accountPayable/infra/http/routes/accountPayable.routes.ts
+++ server/node-js/src/modules/accountPayable/infra/http/routes/accountPayable.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import { CreateAccountsPayableController } from '../controllers/createAccountsPayable/CreateAccountsPayableController'
 import { CreateFixedAccountPayableController } from '../controllers/createFixedAccountPayable/CreateFixedAccountPayableController'
 import { ListAllFixedAccountsByMonthController } from '../controllers/listAllFixedAccountsByMonth/ListAllFixedAccountsByMonthController'
+import { ListAllUnfixedAccountsByMonthController } from '../controllers/listAllUnfixedAccountsByMonth/ListAllUnfixedAccountsByMonthController'
 
 const accounsPayableRoutes = Router()
 
@@ -11,12 +12,18 @@ const createFixedAccountsPayableController =
   new CreateFixedAccountPayableController()
 const listAllFixedAccountsByMonthController =
   new ListAllFixedAccountsByMonthController()
+const listAllUnfixedAccountsByMonthController =
+  new ListAllUnfixedAccountsByMonthController()
 
 accounsPayableRoutes.post('', createAccountsPayableController.handle)
 accounsPayableRoutes.post('/fixed', createFixedAccountsPayableController.handle)
 accounsPayableRoutes.get(
   '/fixed/month',
   listAllFixedAccountsByMonthController.handle,
+)
+accounsPayableRoutes.get(
+  '/unfixed/month',
+  listAllUnfixedAccountsByMonthController.handle,
 )
 
 export { accounsPayableRoutes }
