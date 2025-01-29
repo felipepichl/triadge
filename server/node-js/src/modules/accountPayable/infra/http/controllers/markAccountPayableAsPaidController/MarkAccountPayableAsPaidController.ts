3bbc62f08a63de10d9b7c6fcc7e9d249fa2b6db9
Felipe Pichl <felipe.pichl@hotmail.com>
2025-01-29 11:26:59 -0300

    feat: mark as to paid controller

---
+++ server/node-js/src/modules/accountPayable/infra/http/controllers/markAccountPayableAsPaidController/MarkAccountPayableAsPaidController.ts
@@ -0,0 +1,19 @@
+import { MarkAccountPayableAsPaidUseCase } from '@modules/accountPayable/useCases/markAccountPayableAsPaidUseCase/MarkAccountPayableAsPaidUseCase'
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+class MarkAccountPayableAsPaidController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { accountPayableId } = request.body
+
+    const markAccountPayableAsPaidUseCase = container.resolve(
+      MarkAccountPayableAsPaidUseCase,
+    )
+
+    await markAccountPayableAsPaidUseCase.execute(accountPayableId)
+
+    return response.status(201).send()
+  }
+}
+
+export { MarkAccountPayableAsPaidController }
