00b7f9dad2195e99ed0e9329eec7f3a919e63574
Felipe Pichl <felipe.pichl@hotmail.com>
2025-02-02 12:10:51 -0300

    feat: unpaid account list by month use case

---
+++ server/node-js/src/modules/accountPayable/useCases/listAllUnpaidAccountsByMonth/ListAllUnpaidAccountsByMonthUseCase.ts
@@ -0,0 +1,37 @@
+import { AccountPayable } from '@modules/accountPayable/domain/AccountPayable'
+import { IAccountsPayableRepository } from '@modules/accountPayable/repositories/IAccountsPayableRepository'
+import { calculateAccountsPayableTotals } from '@modules/accountPayable/utils/AccountPayableUtils'
+import { IUseCase } from '@shared/core/domain/IUseCase'
+
+interface IRequest {
+  userId: string
+  month: number
+}
+
+interface IResponse {
+  unpaidAccountsPayable: AccountPayable[]
+  unpaidAccountsPayableTotalAmount: number
+}
+
+class ListAllUnpaidAccountsByMonthUseCase
+  implements IUseCase<IRequest, IResponse>
+{
+  constructor(private accountsPayableRepository: IAccountsPayableRepository) {}
+
+  async execute({ userId, month }: IRequest): Promise<IResponse> {
+    const unpaidAccountsPayable =
+      await this.accountsPayableRepository.listAllUnfixedAccountsByMonth(
+        userId,
+        month,
+      )
+
+    const { total } = calculateAccountsPayableTotals(unpaidAccountsPayable)
+
+    return {
+      unpaidAccountsPayable,
+      unpaidAccountsPayableTotalAmount: total,
+    }
+  }
+}
+
+export { ListAllUnpaidAccountsByMonthUseCase }
