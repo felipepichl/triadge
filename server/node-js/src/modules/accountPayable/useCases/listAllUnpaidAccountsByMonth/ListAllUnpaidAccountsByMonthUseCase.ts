1472418265538397a921ce9df7588b38640605bb
Felipe Pichl <felipe.pichl@hotmail.com>
2025-02-03 15:33:41 -0300

    test: unpaid account list by month e2e

--- server/node-js/src/modules/accountPayable/useCases/listAllUnpaidAccountsByMonth/ListAllUnpaidAccountsByMonthUseCase.ts
+++ server/node-js/src/modules/accountPayable/useCases/listAllUnpaidAccountsByMonth/ListAllUnpaidAccountsByMonthUseCase.ts
@@ -2,6 +2,7 @@ import { AccountPayable } from '@modules/accountPayable/domain/AccountPayable'
 import { IAccountsPayableRepository } from '@modules/accountPayable/repositories/IAccountsPayableRepository'
 import { calculateAccountsPayableTotals } from '@modules/accountPayable/utils/AccountPayableUtils'
 import { IUseCase } from '@shared/core/domain/IUseCase'
+import { inject, injectable } from 'tsyringe'
 
 interface IRequest {
   userId: string
@@ -13,10 +14,14 @@ interface IResponse {
   unpaidAccountsPayableTotalAmount: number
 }
 
+@injectable()
 class ListAllUnpaidAccountsByMonthUseCase
   implements IUseCase<IRequest, IResponse>
 {
-  constructor(private accountsPayableRepository: IAccountsPayableRepository) {}
+  constructor(
+    @inject('AccountsPayableRepository')
+    private accountsPayableRepository: IAccountsPayableRepository,
+  ) {}
 
   async execute({ userId, month }: IRequest): Promise<IResponse> {
     const unpaidAccountsPayable =
