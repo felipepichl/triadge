5d3bcbe6c3c5843fd56d99de0d1b37505af7a39c
Felipe Pichl <felipe.pichl@hotmail.com>
2025-02-04 15:12:10 -0300

    feat: paid account list by month prisma

--- server/node-js/src/modules/accountPayable/infra/prisma/repositories/AccountsPayableRepository.ts
+++ server/node-js/src/modules/accountPayable/infra/prisma/repositories/AccountsPayableRepository.ts
@@ -185,11 +185,25 @@ class AccountsPayableRepository implements IAccountsPayableRepository {
     return AccountPayableMappers.getMapper().toDomainArray(result)
   }
 
-  listAllPaidAccountsByMonth(
+  async listAllPaidAccountsByMonth(
     userId: string,
     month: number,
   ): Promise<AccountPayable[]> {
-    throw new Error('Method not implemented.')
+    const year = new Date().getFullYear()
+    const startDate = new Date(year, month - 1, 1)
+    const endDate = new Date(year, month, 0)
+
+    const result = await PrismaSingleton.getInstance().accountPayable.findMany({
+      where: {
+        userId,
+        AND: [{ dueDate: { gte: startDate } }, { dueDate: { lte: endDate } }],
+        isPaid: true,
+      },
+      include: { financialCategory: true },
+      orderBy: { dueDate: 'desc' },
+    })
+
+    return AccountPayableMappers.getMapper().toDomainArray(result)
   }
 
   async findById(accountPayableId: string): Promise<AccountPayable> {
