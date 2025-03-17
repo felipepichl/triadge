f2abaca9f5782787d58886d62e34e34c48cec9de
Felipe Pichl <felipe.pichl@hotmail.com>
2025-03-17 20:47:43 -0300

    feat: list by type: route

--- server/node-js/src/modules/stock/infra/http/routes/stocks.routes.ts
+++ server/node-js/src/modules/stock/infra/http/routes/stocks.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 
 import { CreateStockController } from '../controllers/createStock/CreateStockController'
+import { ListByTypeController } from '../controllers/listByType/ListByTypeController'
 
 const stocksRoutes = Router()
 
 const createStockController = new CreateStockController()
+const listByType = new ListByTypeController()
 
 stocksRoutes.post('', createStockController.handle)
+stocksRoutes.get('/type', listByType.handle)
 
 export { stocksRoutes }
