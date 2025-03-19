1b4981f8dc71b4223482a715640727a4cfa4e807
Felipe Pichl <felipe.pichl@hotmail.com>
2025-03-19 12:04:07 -0300

    feat: list by type: dependency injection

--- server/node-js/src/modules/stock/useCases/listByType/ListByTypeUseCase.ts
+++ server/node-js/src/modules/stock/useCases/listByType/ListByTypeUseCase.ts
@@ -2,6 +2,7 @@ import { Stock } from '@modules/stock/domain/Stock'
 import { IStockType } from '@modules/stock/domain/StockType'
 import { IStockRepository } from '@modules/stock/repositories/IStockRepository'
 import { IUseCase } from '@shared/core/domain/IUseCase'
+import { inject, injectable } from 'tsyringe'
 
 interface IRequest {
   userId: string
@@ -12,8 +13,12 @@ interface IResponse {
   stocks: Stock[]
 }
 
+@injectable()
 class ListByTypeUseCase implements IUseCase<IRequest, IResponse> {
-  constructor(private stocksRepository: IStockRepository) {}
+  constructor(
+    @inject('StockRepository')
+    private stocksRepository: IStockRepository,
+  ) {}
 
   async execute({ userId, type }: IRequest): Promise<IResponse> {
     const stocks = await this.stocksRepository.listByType(userId, type)
