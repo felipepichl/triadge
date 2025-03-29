4985f81d14561e167b169dd3fe45efb5acb1381f
Felipe Pichl <felipe.pichl@hotmail.com>
2025-03-29 16:24:13 -0300

    feat: get portifolio quotes, dependency injection

--- server/node-js/src/modules/stock/useCases/getPortfolioQuotes/GetPortfolioQuotesUseCase.ts
+++ server/node-js/src/modules/stock/useCases/getPortfolioQuotes/GetPortfolioQuotesUseCase.ts
@@ -3,6 +3,7 @@ import { IB3Provider } from '@modules/stock/providers/B3Provider/models/IB3Provi
 import { IStockRepository } from '@modules/stock/repositories/IStockRepository'
 import { IUseCase } from '@shared/core/domain/IUseCase'
 import { AppError } from '@shared/error/AppError'
+import { inject, injectable } from 'tsyringe'
 
 interface IRequest {
   userId: string
@@ -21,9 +22,12 @@ interface IResponse {
   }[]
 }
 
+@injectable()
 class GetPortfolioQuotesUseCase implements IUseCase<IRequest, IResponse> {
   constructor(
+    @inject('StockRepository')
     private stocksRepository: IStockRepository,
+    @inject('BrapiB3Provider')
     private b3Provider: IB3Provider,
   ) {}
 
