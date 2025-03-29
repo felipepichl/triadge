024e1666e1dce774d70609bdadf19be1645bed9f
Felipe Pichl <felipe.pichl@hotmail.com>
2025-03-29 12:00:43 -0300

    feat: get portifolio quotes, route

--- server/node-js/src/modules/stock/infra/http/routes/stocks.routes.ts
+++ server/node-js/src/modules/stock/infra/http/routes/stocks.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express'
 
 import { CreateStockController } from '../controllers/createStock/CreateStockController'
+import { GetPortfolioQuotesController } from '../controllers/getPortfolioQuotes/GetPortfolioQuotesController'
 import { ListByTypeController } from '../controllers/listByType/ListByTypeController'
 
 const stocksRoutes = Router()
 
 const createStockController = new CreateStockController()
 const listByType = new ListByTypeController()
+const getPortfolioQuotes = new GetPortfolioQuotesController()
 
 stocksRoutes.post('', createStockController.handle)
 stocksRoutes.get('/type', listByType.handle)
+stocksRoutes.get('/portifolio', getPortfolioQuotes.handle)
 
 export { stocksRoutes }
