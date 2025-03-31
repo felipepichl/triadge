444bc4444d51e31ebff8483c0d0057c0d94228d6
Felipe Pichl <felipe.pichl@hotmail.com>
2025-03-31 20:56:51 -0300

    feat: gluestack ui

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,24 +3,27 @@ import {
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto'
-import { StatusBar, Text, View } from 'react-native'
+import { GluestackUIProvider, Text } from '@gluestack-ui/themed'
+import { StatusBar, View } from 'react-native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+    <GluestackUIProvider>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
 
-      {fontsLoaded ? (
-        <Text style={{ fontFamily: 'Roboto_700Bold' }}>Triadge</Text>
-      ) : (
-        <View />
-      )}
-    </View>
+        {fontsLoaded ? (
+          <Text style={{ fontFamily: 'Roboto_700Bold' }}>Triadge</Text>
+        ) : (
+          <View />
+        )}
+      </View>
+    </GluestackUIProvider>
   )
 }
