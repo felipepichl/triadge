b62fbe69911d6807eb2871c6ac8e709f0ac79f69
Felipe Pichl <felipe.pichl@hotmail.com>
2025-03-31 20:45:54 -0300

    feat: status bar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,13 +3,19 @@ import {
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto'
-import { Text, View } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       {fontsLoaded ? (
         <Text style={{ fontFamily: 'Roboto_700Bold' }}>Triadge</Text>
       ) : (
