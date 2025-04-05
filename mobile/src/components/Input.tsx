f4b82f9bddee11b3327a1982e01b7d7dae5c5478
Felipe Pichl <felipe.pichl@hotmail.com>
2025-04-05 20:13:36 -0300

    feat: inputs

--- mobile/src/components/Input.tsx
+++ mobile/src/components/Input.tsx
@@ -1,3 +1,27 @@
-export function Input() {
-  return ()
+import { Input as GluestackInput, InputField } from '@gluestack-ui/themed'
+import { ComponentProps } from 'react'
+
+type InputProps = ComponentProps<typeof InputField>
+
+export function Input({ ...rest }: InputProps) {
+  return (
+    <GluestackInput
+      bg="$gray700"
+      h="$14"
+      px="$4"
+      borderRadius="$xl"
+      borderColor="$gray300"
+      $focus={{
+        borderWidth: 1,
+        borderColor: '$green500',
+      }}
+    >
+      <InputField
+        color="$white"
+        fontFamily="$body"
+        placeholderTextColor="$gray300"
+        {...rest}
+      />
+    </GluestackInput>
+  )
 }
--- mobile/src/screens/auth/SignIn.tsx
+++ mobile/src/screens/auth/SignIn.tsx
@@ -1,5 +1,6 @@
 import BackgroundImage from '@assets/background.png'
 import Logo from '@assets/logo.svg'
+import { Input } from '@components/Input'
 import { Center, Heading, Image, Text, VStack } from '@gluestack-ui/themed'
 
 export function SignIn() {
@@ -14,14 +15,27 @@ export function SignIn() {
         position="absolute"
       />
 
-      <Center my="$24">
-        <Logo />
-      </Center>
+      <VStack flex={1} px="$10" pb="$16">
+        <Center my="$24">
+          <Logo />
+        </Center>
 
-      <Center>
-        <Heading color="$gray100">Acessar Painel</Heading>
-        <Text color="$gray200">Acesse com as suas credenciais</Text>
-      </Center>
+        <Center gap="$8">
+          <Center>
+            <Heading color="$gray100">Acessar Painel</Heading>
+            <Text color="$gray200">Acesse com as suas credenciais</Text>
+          </Center>
+
+          <Center gap="$4">
+            <Input
+              placeholder="Matrícula"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+            <Input placeholder="Senha" secureTextEntry />
+          </Center>
+        </Center>
+      </VStack>
     </VStack>
   )
 }
