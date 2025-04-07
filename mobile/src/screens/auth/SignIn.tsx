002a8a83e16e3eb5471d0dc8ff4ba340d8a13cb4
Felipe Pichl <felipe.pichl@hotmail.com>
2025-04-07 20:21:37 -0300

    feat: signin screen

--- mobile/src/screens/auth/SignIn.tsx
+++ mobile/src/screens/auth/SignIn.tsx
@@ -2,43 +2,55 @@ import BackgroundImage from '@assets/background.png'
 import Logo from '@assets/logo.svg'
 import { Button } from '@components/Button'
 import { Input } from '@components/Input'
-import { Center, Heading, Image, Text, VStack } from '@gluestack-ui/themed'
+import {
+  Center,
+  Heading,
+  Image,
+  ScrollView,
+  Text,
+  VStack,
+} from '@gluestack-ui/themed'
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="$gray700">
-      <Image
-        w="$full"
-        h={624}
-        source={BackgroundImage}
-        defaultSource={BackgroundImage}
-        alt="Pessoas treinando"
-        position="absolute"
-      />
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="$gray700">
+        <Image
+          w="$full"
+          h={624}
+          source={BackgroundImage}
+          defaultSource={BackgroundImage}
+          alt="Pessoas treinando"
+          position="absolute"
+        />
 
-      <Center flex={1} px="$10" pb="$16" gap="$8">
-        <Center my="$24">
-          <Logo />
-        </Center>
-
-        <Center gap="$8">
-          <Center>
-            <Heading color="$gray100">Acessar Painel</Heading>
-            <Text color="$gray200">Acesse com as suas credenciais</Text>
+        <Center flex={1} px="$10" pb="$16" gap="$8">
+          <Center my="$24">
+            <Logo />
           </Center>
 
-          <Center gap="$4">
-            <Input
-              placeholder="Matrícula"
-              keyboardType="email-address"
-              autoCapitalize="none"
-            />
-            <Input placeholder="Senha" secureTextEntry />
-          </Center>
-        </Center>
+          <Center gap="$8">
+            <Center>
+              <Heading color="$gray100">Acessar Painel</Heading>
+              <Text color="$gray200">Acesse com as suas credenciais</Text>
+            </Center>
 
-        <Button title="Acessar Painel" />
-      </Center>
-    </VStack>
+            <Center gap="$4">
+              <Input
+                placeholder="Matrícula"
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
+              <Input placeholder="Senha" secureTextEntry />
+            </Center>
+          </Center>
+
+          <Button title="Acessar Painel" />
+        </Center>
+      </VStack>
+    </ScrollView>
   )
 }
