1c6f5d905640df3fd616cb4add69182ccfad75bf
Felipe Pichl <felipe.pichl@hotmail.com>
2025-04-09 22:57:14 -0300

    feat: some routes to app

---
+++ mobile/src/screens/app/AccountPayable.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from '@gluestack-ui/themed'
+
+export function AccountPayable() {
+  return (
+    <Center flex={1}>
+      <Text>AccountPayable</Text>
+    </Center>
+  )
+}
---
+++ mobile/src/screens/app/Transaction.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from '@gluestack-ui/themed'
+
+export function Transaction() {
+  return (
+    <Center flex={1}>
+      <Text>Transaction</Text>
+    </Center>
+  )
+}
