14b22035cddb3cb25cd3e403292e829eb854135d
Felipe Pichl <felipe.pichl@hotmail.com>
2025-04-09 22:34:00 -0300

    feat: default theme

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,11 +1,18 @@
-import { NavigationContainer } from '@react-navigation/native'
+import { Box } from '@gluestack-ui/themed'
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 
+import { gluestackUIConfig } from '../../config/gluestack-ui.config'
 import { AuthRoutes } from './auth.routes'
 
 export function Routes() {
+  const theme = DefaultTheme
+  theme.colors.background = gluestackUIConfig.tokens.colors.gray700
+
   return (
-    <NavigationContainer>
-      <AuthRoutes />
-    </NavigationContainer>
+    <Box flex={1} bg="$gray700">
+      <NavigationContainer theme={theme}>
+        <AuthRoutes />
+      </NavigationContainer>
+    </Box>
   )
 }
