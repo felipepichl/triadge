5c2216368d8bfcc1c4b7b25b69b11513281481bb
Felipe Pichl <felipe.pichl@hotmail.com>
2025-04-12 09:16:01 -0300

    feat: total invested and current value route

--- server/node-js/src/modules/stock/infra/http/routes/stocks.routes.ts
+++ server/node-js/src/modules/stock/infra/http/routes/stocks.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 
 import { CreateStockController } from '../controllers/createStock/CreateStockController'
 import { GetPortfolioQuotesController } from '../controllers/getPortfolioQuotes/GetPortfolioQuotesController'
+import { GetTotalInvestedAndCurrentQuoteController } from '../controllers/getTotalInvestedAndCurrentQuote/GetTotalInvestedAndCurrentQuoteController'
 import { ListByTypeController } from '../controllers/listByType/ListByTypeController'
 
 const stocksRoutes = Router()
@@ -9,9 +10,12 @@ const stocksRoutes = Router()
 const createStockController = new CreateStockController()
 const listByType = new ListByTypeController()
 const getPortfolioQuotes = new GetPortfolioQuotesController()
+const getTotalInvestedAndCurrentQuote =
+  new GetTotalInvestedAndCurrentQuoteController()
 
 stocksRoutes.post('', createStockController.handle)
 stocksRoutes.get('/type', listByType.handle)
 stocksRoutes.get('/portifolio', getPortfolioQuotes.handle)
+stocksRoutes.get('/investement', getTotalInvestedAndCurrentQuote.handle)
 
 export { stocksRoutes }
