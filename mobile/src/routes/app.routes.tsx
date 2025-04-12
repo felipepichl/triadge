6d82d30d3163f9d7d3ea8bd7fba364a71263a61c
Felipe Pichl <felipe.pichl@hotmail.com>
2025-04-12 20:18:58 -0300

    feat: app routes types

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,13 +1,20 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import {
+  BottomTabNavigationProp,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs'
 import { AccountPayable } from '@screens/app/AccountPayable'
 import { Dashboard } from '@screens/app/Dashboard'
 import { Transaction } from '@screens/app/Transaction'
 
 type AppRoutes = {
-  signIn: undefined
+  dashboard: undefined
+  transaction: undefined
+  accountPayable: undefined
 }
 
-const { Navigator, Screen } = createBottomTabNavigator()
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
 
 export function AppRoutes() {
   return (
--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Box } from '@gluestack-ui/themed'
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 
 import { gluestackUIConfig } from '../../config/gluestack-ui.config'
+import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
 
 export function Routes() {
@@ -11,7 +12,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   )
