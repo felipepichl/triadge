5697459d85e6cd612003aa45d82a721b42036939
Felipe Pichl <felipe.pichl@hotmail.com>
2025-04-12 20:49:09 -0300

    feat: bottom navigator icons

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -4,12 +4,22 @@ import {
 } from '@react-navigation/bottom-tabs'
 import { AccountPayable } from '@screens/app/AccountPayable'
 import { Dashboard } from '@screens/app/Dashboard'
+import { Stock } from '@screens/app/Stock'
 import { Transaction } from '@screens/app/Transaction'
+import {
+  ArrowLeftRightIcon,
+  ChartCandlestick,
+  ScanBarcode,
+  School,
+} from 'lucide-react-native'
+
+import { gluestackUIConfig } from '../../config/gluestack-ui.config'
 
 type AppRoutes = {
   dashboard: undefined
   transaction: undefined
   accountPayable: undefined
+  stock: undefined
 }
 
 export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
@@ -17,6 +27,9 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
 
 export function AppRoutes() {
+  const { tokens } = gluestackUIConfig
+  const iconSize = tokens.space['6']
+
   return (
     <Navigator
       screenOptions={{
@@ -24,9 +37,40 @@ export function AppRoutes() {
         tabBarShowLabel: false,
       }}
     >
-      <Screen name="dashboard" component={Dashboard} />
-      <Screen name="transaction" component={Transaction} />
-      <Screen name="accountPayable" component={AccountPayable} />
+      <Screen
+        name="dashboard"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ color }) => <School size={iconSize} color={color} />,
+        }}
+      />
+      <Screen
+        name="transaction"
+        component={Transaction}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ArrowLeftRightIcon size={iconSize} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="accountPayable"
+        component={AccountPayable}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ScanBarcode size={iconSize} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="stock"
+        component={Stock}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ChartCandlestick size={iconSize} color={color} />
+          ),
+        }}
+      />
     </Navigator>
   )
 }
---
+++ mobile/src/screens/app/Stock.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from '@gluestack-ui/themed'
+
+export function Stock() {
+  return (
+    <Center flex={1}>
+      <Text>Stock</Text>
+    </Center>
+  )
+}
