39922a3bb11f40cc72ca90339f031abd88161f5f
Felipe Pichl <felipe.pichl@hotmail.com>
2025-04-12 15:47:48 -0300

    wip: summaries to stock

--- web/vite-web/src/pages/app/stock.tsx
+++ web/vite-web/src/pages/app/stock.tsx
@@ -1,10 +1,16 @@
-import { useCallback, useEffect } from 'react'
+import { Activity, DollarSign, TrendingUpDown } from 'lucide-react'
+import { useCallback, useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 
 import { NewStock } from '@/components/new-stock/new-stock'
+import { SummaryProps } from '@/components/summary/summary'
+import { SummaryCarousel } from '@/components/summary/summary-carousel'
 import { useStock } from '@/hooks/use-stock'
+// import { priceFormatter } from '@/util/formatter'
 
 export function Stock() {
+  const [summaries, setSummaries] = useState<SummaryProps[]>([])
+
   const { getPortfolioQuotes } = useStock()
 
   const portfolioQuotes = useCallback(async () => {
@@ -15,10 +21,43 @@ export function Stock() {
     portfolioQuotes()
   }, [portfolioQuotes])
 
+  useEffect(() => {
+    const summariesResume: SummaryProps[] = [
+      {
+        color: 'default',
+        description: 'Total Investido',
+        icon: DollarSign,
+        iconColor: '#00b37e',
+        value: 'R$ 1.000,00',
+        // priceFormatter.format(
+        //   transactionByDateRangeAndType.balance?.income ?? 0,
+        // ),
+      },
+      {
+        color: 'default',
+        description: 'Cotação Atual',
+        icon: Activity,
+        iconColor: '#ff0000',
+        value: 'R$ 1.250,00',
+      },
+      {
+        color: 'green',
+        description: 'Posição',
+        icon: TrendingUpDown,
+        iconColor: '#fff',
+        value: 'R$ 250,00',
+      },
+    ]
+
+    setSummaries(summariesResume)
+  }, [])
+
   return (
     <>
       <Helmet title="Ações e FIIs" />
       <NewStock />
+
+      <SummaryCarousel summaries={summaries} />
     </>
   )
 }
