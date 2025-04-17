2fba17b06719cb70d9ac735341a2672b236422c7
Felipe Pichl <felipe.pichl@hotmail.com>
2025-04-17 19:29:59 -0300

    wip: month select

--- mobile/src/components/DashboardHeader.tsx
+++ mobile/src/components/DashboardHeader.tsx
@@ -1,27 +1,80 @@
-import { Heading, HStack, Icon, Text, VStack } from '@gluestack-ui/themed'
+import {
+  Box,
+  Center,
+  Heading,
+  HStack,
+  Icon,
+  Text,
+  VStack,
+} from '@gluestack-ui/themed'
 import { LogOut } from 'lucide-react-native'
+import { useCallback } from 'react'
+import { VictoryPie } from 'victory-native'
 
+import { MonthSelect } from './MonthSelect'
 import { UserPhoto } from './UserPhoto'
 
 export function DashboardHeader() {
+  const handleMonthSelect = useCallback(async (monthNumber: string) => {
+    console.log(monthNumber)
+  }, [])
+
   return (
-    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
-      <UserPhoto
-        source={{ uri: 'https://github.com/felipepichl.png' }}
-        w="$16"
-        h="$16"
-      />
+    <VStack>
+      <HStack
+        bg="$gray600"
+        pt="$16"
+        pb="$5"
+        px="$8"
+        alignItems="center"
+        gap="$4"
+      >
+        <UserPhoto
+          source={{ uri: 'https://github.com/felipepichl.png' }}
+          w="$16"
+          h="$16"
+        />
 
-      <VStack flex={1}>
-        <Text color="$gray100" fontSize="$sm">
-          Olá,
-        </Text>
-        <Heading color="$gray100" fontSize="$md">
-          Felipe Pichl
+        <VStack flex={1}>
+          <Text color="$gray100" fontSize="$sm">
+            Olá,
+          </Text>
+          <Heading color="$gray100" fontSize="$md">
+            Felipe Pichl
+          </Heading>
+        </VStack>
+
+        <Icon as={LogOut} color="$red500" size="xl" />
+      </HStack>
+
+      <Box px="$8" pt="$5" pb="$5">
+        <Heading color="$gray100" fontSize="$xl">
+          Transações
         </Heading>
-      </VStack>
-
-      <Icon as={LogOut} color="$red500" size="xl" />
-    </HStack>
+        <Text color="$gray300" fontSize="$sm">
+          Por entrada e saída
+        </Text>
+      </Box>
+      <MonthSelect onMonthSelect={handleMonthSelect} />
+      <Center mt="$5">
+        <VictoryPie
+          data={[
+            { x: 'Entrada', y: 97 },
+            { x: 'Saída', y: 35 },
+          ]}
+          colorScale={['green', 'red']}
+          width={300}
+          height={300}
+          labelRadius={60}
+          style={{
+            labels: {
+              fill: 'white',
+              fontSize: 18,
+            },
+          }}
+          padding={{ top: 0, bottom: 0, left: 0, right: 0 }}
+        />
+      </Center>
+    </VStack>
   )
 }
---
+++ mobile/src/components/MonthSelect.tsx
@@ -0,0 +1,84 @@
+import {
+  Select,
+  SelectBackdrop,
+  SelectContent,
+  SelectDragIndicator,
+  SelectDragIndicatorWrapper,
+  SelectIcon,
+  SelectInput,
+  SelectItem,
+  SelectPortal,
+  SelectScrollView,
+  SelectTrigger,
+} from '@gluestack-ui/themed'
+import { ChevronDownIcon } from 'lucide-react-native'
+import { useCallback } from 'react'
+
+type MonthSelectProps = {
+  onMonthSelect: (monthNumber: string) => void
+  defaultMonth?: number
+}
+
+export function MonthSelect({ onMonthSelect, defaultMonth }: MonthSelectProps) {
+  const months = [...Array(12).keys()].map((i) => ({
+    value: String(i + 1),
+    label: new Date(2020, i).toLocaleString('pt-BR', {
+      month: 'long',
+    }),
+  }))
+
+  const currentMonth = defaultMonth || new Date().getMonth() + 1
+  const selectedValue = String(currentMonth)
+  const selectedLabel = months.find((m) => m.value === selectedValue)?.label
+
+  const handleSelectChange = useCallback(
+    (monthNumber: string) => {
+      onMonthSelect(monthNumber)
+    },
+    [onMonthSelect],
+  )
+
+  return (
+    <Select
+      px="$8"
+      onValueChange={handleSelectChange}
+      // selectedValue={selectedValue}
+      defaultValue={selectedValue}
+      initialLabel={selectedLabel}
+    >
+      <SelectTrigger borderRadius="$xl" h="$14" borderColor="$gray400">
+        <SelectInput color="$gray100" />
+        <SelectIcon as={ChevronDownIcon} mr="$2" />
+      </SelectTrigger>
+      <SelectPortal>
+        <SelectBackdrop />
+        <SelectContent bg="$gray600">
+          <SelectDragIndicatorWrapper>
+            <SelectDragIndicator bg="$gray400" />
+          </SelectDragIndicatorWrapper>
+          <SelectScrollView showsVerticalScrollIndicator={false}>
+            {months.map((month) => (
+              <SelectItem
+                key={month.value}
+                value={month.value}
+                label={month.label}
+                textStyle={{
+                  color: '$gray100',
+                }}
+                $pressed-bg="$gray700"
+                $active-bg="$gray700"
+                $active-bgColor="$gray700"
+                $active-backgroundColor="$gray700"
+                $active-borderRadius="$xl"
+                $checked-bg="$gray700"
+                $checked-bgColor="$gray700"
+                $checked-backgroundColor="$gray700"
+                $checked-borderRadius="$xl"
+              />
+            ))}
+          </SelectScrollView>
+        </SelectContent>
+      </SelectPortal>
+    </Select>
+  )
+}
