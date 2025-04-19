6402aeca71c28a930e0d938c1420341e69d3fb65
Felipe Pichl <felipe.pichl@hotmail.com>
2025-04-19 20:40:38 -0300

    wip: bar chart

---
+++ mobile/src/components/charts/BarChart.tsx
@@ -0,0 +1,78 @@
+import { gluestackUIConfig } from '@gluestack-ui/config'
+import { Center } from '@gluestack-ui/themed'
+import {
+  VictoryAxis,
+  VictoryBar,
+  VictoryChart,
+  VictoryLabel,
+  VictoryTheme,
+} from 'victory-native'
+
+export function BarChart() {
+  const barColor = gluestackUIConfig.tokens.colors.green500
+
+  const chartData = [
+    { x: 'Categoria A', y: 4 },
+    { x: 'Categoria B', y: 3 },
+    { x: 'Categoria C', y: 2 },
+    { x: 'Categoria D', y: 5 },
+    { x: 'Categoria E', y: 8 },
+    { x: 'Categoria F', y: 5 },
+    { x: 'Categoria G', y: 9 },
+    { x: 'Categoria H', y: 2 },
+  ]
+
+  return (
+    <Center px="$8" pt="$2" pb="$2" alignItems="flex-start">
+      <VictoryChart
+        width={300}
+        height={Math.min(Math.max(chartData.length * 62 + 32, 130), 600)}
+        padding={{ top: 48, left: 0, bottom: 48 }}
+        theme={VictoryTheme.clean}
+        animate={{
+          duration: 1000,
+          easing: 'exp',
+          onLoad: { duration: 1000 },
+        }}
+      >
+        <VictoryAxis
+          style={{
+            axis: { stroke: 'none' },
+            ticks: { stroke: 'none' },
+            tickLabels: { fill: 'none' },
+            grid: { stroke: 'none' },
+          }}
+        />
+
+        <VictoryBar
+          horizontal
+          data={chartData}
+          labels={({ datum }) => datum.x}
+          labelComponent={
+            <VictoryLabel
+              x={10}
+              textAnchor="start"
+              style={{
+                fill: 'white',
+                fontSize: 14,
+                fontWeight: 'bold',
+              }}
+            />
+          }
+          style={{
+            data: {
+              fill: barColor,
+              stroke: 'none',
+              fillOpacity: 0.4,
+            },
+            labels: {
+              fontSize: 12,
+            },
+          }}
+          barWidth={58}
+          cornerRadius={{ top: 8, bottom: 8 }}
+        />
+      </VictoryChart>
+    </Center>
+  )
+}
--- mobile/src/screens/app/Dashboard.tsx
+++ mobile/src/screens/app/Dashboard.tsx
@@ -1,7 +1,8 @@
+import { BarChart } from '@components/charts/BarChart'
 import { PieChart } from '@components/charts/PieChart'
 import { DashboardHeader } from '@components/DashboardHeader'
 import { MonthSelect } from '@components/MonthSelect'
-import { Box, Heading, Text, VStack } from '@gluestack-ui/themed'
+import { Box, Heading, ScrollView, Text, VStack } from '@gluestack-ui/themed'
 import { useCallback } from 'react'
 
 export function Dashboard() {
@@ -13,18 +14,36 @@ export function Dashboard() {
     <VStack flex={1}>
       <DashboardHeader />
 
-      <Box px="$8" pt="$5" pb="$5">
-        <Heading color="$gray100" fontSize="$xl">
-          Transações
-        </Heading>
-        <Text color="$gray300" fontSize="$sm">
-          Por entrada e saída
-        </Text>
-      </Box>
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <Box px="$8" pt="$5" pb="$5">
+          <Heading color="$gray100" fontSize="$xl">
+            Transações
+          </Heading>
+          <Text color="$gray300" fontSize="$sm">
+            Por entrada e saída
+          </Text>
+        </Box>
 
-      <MonthSelect onMonthSelect={handleMonthSelect} />
+        <MonthSelect onMonthSelect={handleMonthSelect} />
 
-      <PieChart income={30} outcome={70} />
+        <PieChart income={30} outcome={70} />
+
+        <Box px="$8" pt="$5" pb="$5">
+          <Heading color="$gray100" fontSize="$xl">
+            Categorias
+          </Heading>
+          <Text color="$gray300" fontSize="$sm">
+            Transações de saída
+          </Text>
+        </Box>
+
+        <MonthSelect onMonthSelect={handleMonthSelect} />
+
+        <BarChart />
+      </ScrollView>
     </VStack>
   )
 }
