6428d5491bae3009b8f561466fbd94487cedb288
Felipe Pichl <felipe.pichl@hotmail.com>
2025-04-21 14:12:19 -0300

    refactor: dashboard header

--- mobile/src/components/DashboardHeader.tsx
+++ mobile/src/components/DashboardHeader.tsx
@@ -5,32 +5,23 @@ import { UserPhoto } from './UserPhoto'
 
 export function DashboardHeader() {
   return (
-    <VStack>
-      <HStack
-        bg="$gray600"
-        pt="$16"
-        pb="$5"
-        px="$8"
-        alignItems="center"
-        gap="$4"
-      >
-        <UserPhoto
-          source={{ uri: 'https://github.com/felipepichl.png' }}
-          w="$16"
-          h="$16"
-        />
+    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
+      <UserPhoto
+        source={{ uri: 'https://github.com/felipepichl.png' }}
+        w="$16"
+        h="$16"
+      />
 
-        <VStack flex={1}>
-          <Text color="$gray100" fontSize="$sm">
-            Olá,
-          </Text>
-          <Heading color="$gray100" fontSize="$md">
-            Felipe Pichl
-          </Heading>
-        </VStack>
+      <VStack flex={1}>
+        <Text color="$gray100" fontSize="$sm">
+          Olá,
+        </Text>
+        <Heading color="$gray100" fontSize="$md">
+          Felipe Pichl
+        </Heading>
+      </VStack>
 
-        <Icon as={LogOut} color="$red500" size="xl" />
-      </HStack>
-    </VStack>
+      <Icon as={LogOut} color="$red500" size="xl" />
+    </HStack>
   )
 }
