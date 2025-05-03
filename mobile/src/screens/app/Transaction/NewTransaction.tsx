35ea6c343c5622de896f3cdaa387f15ea02a3f0e
Felipe Pichl <felipe.pichl@hotmail.com>
2025-05-03 13:57:13 -0300

    feat: monetary mask

--- mobile/src/screens/app/Transaction/NewTransaction.tsx
+++ mobile/src/screens/app/Transaction/NewTransaction.tsx
@@ -3,15 +3,19 @@ import { DatePicker } from '@components/GenericFormAndFiled/Fileds/DatePicker'
 import { StackHeader } from '@components/Headers/StackHeader'
 import { Input } from '@components/Input'
 import { VStack } from '@gluestack-ui/themed'
+import { useMonetaryMask } from '@hooks/useMonetaryMask'
+import { NativeSyntheticEvent, TextInputChangeEventData } from 'react-native'
+
+const categorias = Array.from({ length: 8 }, (_, index) => {
+  const letra = String.fromCharCode(65 + index) // 65 = 'A'
+  return {
+    _id: String(index),
+    description: `Categoria ${letra}`,
+  }
+})
 
 export function NewTransaction() {
-  const categorias = Array.from({ length: 8 }, (_, index) => {
-    const letra = String.fromCharCode(65 + index) // 65 = 'A'
-    return {
-      _id: String(index),
-      description: `Categoria ${letra}`,
-    }
-  })
+  const { formattedValue, handleMaskChange } = useMonetaryMask()
 
   return (
     <VStack flex={1}>
@@ -19,7 +23,14 @@ export function NewTransaction() {
 
       <VStack p="$4" gap="$4">
         <Input placeholder="Descrição" />
-        <Input placeholder="Valor" />
+        <Input
+          placeholder="Valor"
+          keyboardType="numeric"
+          value={formattedValue}
+          onChange={(e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+            handleMaskChange(e)
+          }}
+        />
         <DatePicker />
         <CategorySelect type="financialCategory" options={categorias} />
         <CategorySelect type="subcategory" options={categorias} />
