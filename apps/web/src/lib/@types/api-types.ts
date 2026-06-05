03564ddb6b357a64a7c42a9dc8944b419f3676e6
felipepichl <felipe.pichl@hotmail.com>
2026-06-05 11:10:11 -0300

    refactor(web): remove dead auth files superseded by @umabel/core

--- apps/web/src/lib/@types/api-types.ts
+++
@@ -1,7 +0,0 @@
-import { AxiosInstance } from 'axios'
-
-export type SignOut = () => void
-
-export type APIInstanceProps = AxiosInstance & {
-  registerInterceptTokenManager: (signOut: SignOut) => () => void
-}
